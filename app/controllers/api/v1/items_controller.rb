ab1336a65aea89b5c90c1853fa832b03990ffa12
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-22 18:11:08 -0600

    Add show method

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::ItemsController < ApplicationController
     @all_items = Item.paginations(params[:page], params[:per_page])
     render json: ItemSerializer.new(@all_items)
   end
+
+  def show
+    @item = Item.find(params[:id])
+    render json: ItemSerializer.new(@item)
+  end
 end
