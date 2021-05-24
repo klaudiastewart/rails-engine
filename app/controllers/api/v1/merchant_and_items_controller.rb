70c85fd536d32853fc7b7778e9e2918740f5119c
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-23 18:59:36 -0600

    Update show method to include item_id params

--- app/controllers/api/v1/merchant_and_items_controller.rb
+++ app/controllers/api/v1/merchant_and_items_controller.rb
@@ -1,7 +1,15 @@
 class Api::V1::MerchantAndItemsController < ApplicationController
   def show
-    @merchant = Merchant.find(params[:merchant_id])
-    @merchant_items = @merchant.items
-    render json: ItemSerializer.new(@merchant_items)
+    # require "pry"; binding.pry
+    if params.include?(:merchant_id)
+      @merchant = Merchant.find(params[:merchant_id])
+      @merchant_items = @merchant.items
+      render json: ItemSerializer.new(@merchant_items)
+    else
+      @item = Item.find(params[:item_id])
+      @item_merchant = @item.merchant
+      render json: MerchantSerializer.new(@item_merchant)
+    end
+    #does this automatically send a 404 if the id isn't in db?
   end
 end
