2012bb97790a8a7ecd722bc660266b878e1922f8
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-24 17:15:14 -0600

    Add conditionals for 404

--- app/controllers/api/v1/merchant_and_items_controller.rb
+++ app/controllers/api/v1/merchant_and_items_controller.rb
@@ -1,15 +1,22 @@
 class Api::V1::MerchantAndItemsController < ApplicationController
   def show
-    # require "pry"; binding.pry
+    record_not_found("No id entered") if !params[:merchant_id] && !params[:item_id]
     if params.include?(:merchant_id)
-      @merchant = Merchant.find(params[:merchant_id])
-      @merchant_items = @merchant.items
-      render json: ItemSerializer.new(@merchant_items)
+      if !Merchant.find_by_id(params[:merchant_id]).nil?
+        @merchant = Merchant.find(params[:merchant_id])
+        @merchant_items = @merchant.items
+        render json: ItemSerializer.new(@merchant_items)
+      else
+        return record_not_found("Record not found")
+      end
     else
-      @item = Item.find(params[:item_id])
-      @item_merchant = @item.merchant
-      render json: MerchantSerializer.new(@item_merchant)
+      if !Item.find_by_id(params[:item_id]).nil?
+        @item = Item.find_by_id(params[:item_id])
+        @item_merchant = @item.merchant
+        render json: MerchantSerializer.new(@item_merchant)
+      else
+        return record_not_found("Item record not found")
+      end
     end
-    #does this automatically send a 404 if the id isn't in db?
   end
 end
