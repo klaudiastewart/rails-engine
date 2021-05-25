7929ca2bd0ad113069a4535a0607bade8ffd49ad
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-25 15:58:13 -0600

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      # get '/merchants', to: "merchants#index"
+      get '/merchants/find_all', to: "merchants/search#index"
+      get '/merchants/find', to: "merchants/search#show"
+
+      get '/items/find', to: 'items/search#show'
+      get '/items/find_all', to: "items/search#index"
       resources :merchants, only: [:index, :show] do
         get '/items', to: "merchant_and_items#show"
       end
