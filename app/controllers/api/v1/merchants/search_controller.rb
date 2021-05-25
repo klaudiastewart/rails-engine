5a75b3713fbaefd87be72703a8bffbb623279ca0
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-25 15:57:50 -0600

    Change index and show methods

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,10 +1,15 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def index
-    @all_merchants = Merchant.merchant_names #(params[:name])
+    @all_merchants = Merchant.merchants(params[:name])
     render json: MerchantSerializer.new(@all_merchants)
   end
 
-  # def show
-  #   @merchant = Merchant.merchant_names(params[:name])
-  # end
+  def show
+    @merchant = Merchant.merchant(params[:name])
+    if @merchant.present?
+      render json: MerchantSerializer.new(@merchant)
+    else
+      return record_not_found("This merchant does not exist")
+    end
+  end
 end
