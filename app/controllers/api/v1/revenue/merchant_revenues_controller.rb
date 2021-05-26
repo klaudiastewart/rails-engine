c2823e4a7f045c14106cf0a7cdc0a16baae4ce95
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-26 16:32:05 -0600

    Add show method

--- app/controllers/api/v1/revenue/merchant_revenues_controller.rb
+++ app/controllers/api/v1/revenue/merchant_revenues_controller.rb
@@ -4,4 +4,10 @@ class Api::V1::Revenue::MerchantRevenuesController < ApplicationController
     @merchants = Merchant.total_revenue(params[:quantity])
     render json: MerchantNameRevenueSerializer.new(@merchants)
   end
+
+  def show
+    return record_not_found("Bad id entered") if params[:id].to_i == 0 || !params[:id].present?
+    @merchant = Merchant.find(params[:id])
+    render json: MerchantRevenueSerializer.new(@merchant)
+  end
 end
