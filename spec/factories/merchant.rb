d8c869704cf0bd96ff0acb637e6a677198d6642a
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-26 17:17:46 -0600

    Refactor merchant factory

--- spec/factories/merchant.rb
+++ spec/factories/merchant.rb
@@ -1,10 +1,5 @@
-# FactoryBot.define do
-#   factory :merchant do
-#     sequence :id do |i|
-#       "#{i}"
-#     end
-#     sequence :name do |n|
-#       "Merchant #{Faker::Name.name}"
-#     end
-#   end
-# end
+FactoryBot.define do
+  factory :merchant do
+    name {Faker::Name.name}
+  end
+end
