7b80cf95e53e922e73299e99c26d22072c43778f
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-26 15:40:59 -0600

    Add error handling method for 400

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,15 @@ class ApplicationController < ActionController::API
     render json: JSON.parse(body, :quirks_mode => true), status: 404
   end
 
+  def bad_params_400(message)
+    body = ({
+      data: {},
+      error: "#{message}",
+      status: 400
+      }).to_json
+    render json: JSON.parse(body, :quirks_mode => true), status: 400
+  end
+
   def record_not_found(message)
     bad_params(message)
   end
