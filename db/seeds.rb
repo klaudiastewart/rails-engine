57b7ae71eb8d9a80fdec89555c0e89c3b820ae11
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-28 14:13:25 -0600

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+# cmd = "pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $(whoami) -d rails-engine_development db/data/rails-engine-development.pgdump"
+# puts "Loading PostgreSQL Data dump into local database with command:"
+# puts cmd
+# system(cmd)
 
 Customer.create!([
   {first_name: "Joey", last_name: "Ondricka"},
