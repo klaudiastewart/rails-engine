5d3eba713cc1fff312c8e8668ee0285b93837205
Klaudia Stewart <klaudiastewart@gmail.com>
2021-05-28 12:27:16 -0600

    Add config.assets.initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module RailsEngine
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.assets.initialize_on_precompile = false
   end
 end
